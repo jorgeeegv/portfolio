bd69643c0e55bcfdfd6d19190f4746ec4dd48e83
xNySO <jorgegvilla98@gmail.com>
2023-04-20 22:53:46 +0200

    added submit actions at contact form and control errors

--- src/Core/components/Contact/Contact.tsx
+++ src/Core/components/Contact/Contact.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { emailConfig } from '../../../constants/emailjs.constants';
 import './Contact.css'
+import "react-toastify/ReactToastify.min.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
 import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
 
 interface ContactForm {
     name: any;
@@ -12,40 +14,85 @@ interface ContactForm {
     message: any;
 }
 export const ContactUs = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = (data: any) => console.log(errors);
+    const { register, handleSubmit, reset, formState: { errors, isValid } } = useForm({ mode: 'onChange' });
+    const [blockForm, setBlockForm] = useState(false);
+    const form = useRef<any>(null);
+    const onSubmit = (data: any) => {
+        emailjs.sendForm(emailConfig.SERVICE_ID, emailConfig.TEMPLATE_ID, form.current, emailConfig.PUBLIC_KEY)
+        .then((result) => {
+            reset();
+            setBlockForm(true);
+            toast.success('Mensaje enviado correctamente!', {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                progress: undefined,
+                theme: "colored"
+            });
+        }, (error) => {
+            toast.success('Error al enviar el mensaje', {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                progress: undefined,
+                theme: "colored"
+            });
+        });
+    }
+    useEffect(()=>{
+        
+    },[blockForm])
     return (
         <div className='w-full lg:w-1/4 mt-20 mb-20 mx-auto'>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
+            <form onSubmit={handleSubmit(onSubmit)} ref={(e) => { form.current = e}}>
                 <div className="pb-12 w-full">
                     <h2 className="text-3xl sm:mx-auto font-semibold leading-7 color-primary">Contáctame</h2>
                     <div className="mt-10 flex flex-col w-full gap-y-5">
                         <div className="">
-                            <label htmlFor="first-name" className={(errors.user_name ? 'color-danger': '') + " block pl-1 text-1xl font-semibold color-primary"}>
+                            <label htmlFor="first-name" className={(errors.user_name ? 'color-danger' : '') + " block pl-1 text-1xl font-semibold color-primary"}>
                                 Nombre
                             </label>
                             <div className="mt-2">
                                 <input
                                     type="text"
                                     id="user_name"
-                                    className={ (errors.user_name ? 'error-input': '') + " primary-input block w-full rounded-md py-1.5 sm:text-sm sm:leading-6 "}
+                                    className={(errors.user_name ? 'error-input' : '') + " primary-input block w-full rounded-md py-1.5 sm:text-sm sm:leading-6 "}
                                     placeholder="Nombre Apellido..."
-                                    {...register("user_name", { required: 'El nombre es requerido' })}
+                                    {...register("user_name", { disabled: blockForm, required: 'El nombre es requerido' })}
                                 />
-                                {errors.user_name && <small role='alert' className='color-danger'>{ errors.user_name.message?.toString()}</small>}
+                                {errors.user_name && <small role='alert' className='color-danger p-1'>{errors.user_name.message?.toString()}</small>}
                             </div>
                         </div>
                         <div className="">
-                            <label htmlFor="email" className={(errors.user_email ? 'color-danger': '') + " block text-1xl font-semibold color-primary "}>
+                            <label htmlFor="email" className={(errors.user_email ? 'color-danger' : '') + " block text-1xl font-semibold color-primary "}>
                                 Email
                             </label>
                             <div className="mt-2">
                                 <input
                                     type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
                                     id="user_email"
-                                    className={(errors.user_email ? 'error-input': '') + " primary-input block w-full rounded-md border-0 py-1.5  sm:text-sm sm:leading-6 "}
+                                    className={(errors.user_email ? 'error-input' : '') + " primary-input block w-full rounded-md border-0 py-1.5  sm:text-sm sm:leading-6 "}
                                     placeholder="ejemplo@ejemplo.com"
                                     {...register("user_email", {
+                                        disabled: blockForm,
                                         required: 'El email es requerido',
                                         pattern: {
                                             value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
@@ -53,7 +100,7 @@ export const ContactUs = () => {
                                         }
                                     })}
                                 />
-                                {errors.user_email && <small role='alert' className='color-danger'>{ errors.user_email.message?.toString()}</small>}
+                                {errors.user_email && <small role='alert' className='color-danger p-1'>{errors.user_email.message?.toString()}</small>}
 
                             </div>
                         </div>
@@ -63,15 +110,15 @@ export const ContactUs = () => {
                             </label>
                             <div className="mt-2">
                                 <textarea placeholder='Mensaje...'
-                                    {...register("user_message")}
-                                    className={(errors.user_message ? 'error-input': '') + " primary-input block w-full rounded-md  sm:text-sm sm:leading-6 "}></textarea>
-                               {errors.user_message && <small role='alert' className='color-danger'>{ errors.user_message.message?.toString()}</small>}
+                                    {...register("user_message", {disabled: blockForm})}
+                                    className={(errors.user_message ? 'error-input' : '') + " primary-input block w-full rounded-md  sm:text-sm sm:leading-6 "}></textarea>
+                                {errors.user_message && <small role='alert' className='color-danger'>{errors.user_message.message?.toString()}</small>}
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className='w-full mx-auto'>
-                    <button className={ (errors ? 'cta-invalid' : 'cta') + " w-full send-text"} type="submit">
+                    <button disabled={!isValid || blockForm} className={((!isValid || blockForm) ? 'cta-invalid' : 'cta') + " w-full send-text"} type="submit">
                         <span className='text-2xl mx-auto font-bold flex gap-x-2'>ENVIAR
                             <FontAwesomeIcon icon={faPaperPlane} className='' />
                         </span>
